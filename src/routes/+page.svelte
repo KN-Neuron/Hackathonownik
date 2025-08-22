c63aad4b8f9674321752852a973845bee221a72e
Ernest Gupik <ernestgupik@wp.pl>
2025-08-22 19:52:05 +0200

    Update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import "../app.css";
+  import { pb } from '$lib/pocketbase';
+  import { onMount } from 'svelte';
+  import Loading from '$lib/Loading.svelte';
+  let user = null;
+  let loading = true;
+
+  onMount(async () => {
+    user = pb.authStore.model;
+    loading = false;
+  });
+
+  function logout() {
+    pb.authStore.clear();
+    user = null;
+  }
+</script>
+
+<h1 class="text-4xl">
+    Welcome
+    {#if loading}
+        <Loading />
+    {:else if user}
+        {user.email}
+    {:else}
+        Guest
+    {/if}
+</h1>
+
+<nav>
+    {#if loading}
+      <!-- <Loading /> -->
+      <a href="/login" class="btn">Login</a> |
+      <a href="/register" class="btn">Register</a>
+    {:else}
+        {#if !user}
+          <a href="/login" class="btn">Login</a> |
+          <a href="/register" class="btn">Register</a>
+        {:else}
+          <a href="/protected" class="btn">Protected Page</a> |
+          <button on:click={logout} class="btn">Logout</button>
+        {/if}
+    {/if}
+</nav>
